530513589f876f2ce93dc27bc9588ad46e5d95b1
Youran <sengyouran2001@gmail.com>
2026-04-25 10:32:54 +0700

    add real url backend

--- src/Context.jsx
+++ src/Context.jsx
@@ -71,7 +71,7 @@ function Context({ children }) {
     start_time: "",
     end_time: "",
   });
-  const url = "http://localhost:3000";
+  const url = "my-backend-production-b2d1.up.railway.app";
   const {
     loginFalse,
     setLoginFalse,
